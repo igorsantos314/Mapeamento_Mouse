375794d4dedb758ce8700ddccc887c4d6d7ac629
Igor Santos <igorag314@gmail.com>
2021-05-22 08:42:10 -0300

    Macro

--- map.py
+++ map.py
@@ -2,6 +2,7 @@ from ctypes import windll, Structure, c_long, byref
 from time import sleep
 from tkinter import *
 from tkinter import Frame
+import _thread as th
 
 class POINT(Structure):
     _fields_ = [("x", c_long), ("y", c_long)]
@@ -12,19 +13,25 @@ class mapa(Frame):
         
         self.difX = 0
         self.difY = 0
+        self.statusMacro = False
 
         self.window = Tk()
         self.window.title('MAPEAMENTO')
+        self.window.geometry('250x420')
 
         lbl = Label(text="MAPEAMENTO DA CIDADE:")
         lbl.pack()
 
         #POSIÇÃO
-        self.etPos = Entry(font='Courier 12')
+        self.etPos = Entry(font='Courier 12', bg='black', fg='green')
         self.etPos.pack()
 
         self.lblPosAtual = Label(font='Courier 12', fg='red')
         self.lblPosAtual.pack()
+
+        #MACRO
+        self.lblMacro = Label(text='  MACRO OFF  ', font='Courier 20', bg='black', fg='red')
+        self.lblMacro.pack()
 
         #BOTAO PARA GERAR CÓDIGO
         btCod = Button(text='GERAR CÓDIGO HTML', command=lambda: setCod())
@@ -46,7 +53,7 @@ class mapa(Frame):
         lblCod = Label(text="CÓDIGO HTML:")
         lblCod.pack()
 
-        self.etCodigo = Entry(font='Courier 12')
+        self.etCodigo = Entry(font='Courier 12', bg='black', fg='green')
         self.etCodigo.pack()
 
         def setCod():
@@ -97,6 +104,7 @@ class mapa(Frame):
 
         #AO PRESSIONAR P DISPARAR O EVENTO
         self.window.bind("<F1>", self.keyPressed)
+        self.window.bind("<F4>", self.keyPressed)
         self.window.bind("<KeyPress-c>", self.keyPressed)
 
         self.window.mainloop()
@@ -116,14 +124,8 @@ class mapa(Frame):
         l = event.keysym
 
         if l == 'F1':
-            #ATIVAR EVENTO
-            pos = self.queryMousePosition(False)
-
-            #EXIBIR POSIÇÃO SELECIONADA
-            self.lblPosAtual['text'] = "X=" + str(pos["x"]) + " Y:" + str(pos["y"])
-
-            #GUARDAR POSIÇÃO
-            self.etPos.insert(END, str(pos["x"]) + "," + str(pos["y"]) + ",")
+            #CHAMAR MACRO
+            self.macro()
 
         elif l == 'c':
             #ATIVAR EVENTO
@@ -132,5 +134,41 @@ class mapa(Frame):
             #INSERIR POSIÇÕES
             self.etCalibX.insert(0, str(pos["x"]))
             self.etCalibY.insert(0, str(pos["y"]))
+
+        elif l == 'F4':
+
+            if self.statusMacro:
+                self.statusMacro = False
+                self.lblMacro['text'] = '  MACRO OFF  '
+                self.lblMacro['fg'] = 'red'
+
+            else:
+                self.statusMacro = True
+                self.lblMacro['text'] = '  MACRO ON  '
+                self.lblMacro['fg'] = 'green'
+                  
+                th.start_new_thread(self.startMacro, ())
+
+    def startMacro(self):
+        while True:
+
+            if self.statusMacro:
+                #CHAMAR MACRO
+                self.macro()
+                sleep(1)
+
+            else:
+                break
+
+    def macro(self):
+
+         #ATIVAR EVENTO
+        pos = self.queryMousePosition(False)
+
+        #EXIBIR POSIÇÃO SELECIONADA
+        self.lblPosAtual['text'] = "X=" + str(pos["x"]) + " Y:" + str(pos["y"])
+
+        #GUARDAR POSIÇÃO
+        self.etPos.insert(END, str(pos["x"]) + "," + str(pos["y"]) + ",")
 
 mapa()
