3794d5a00a4ca8a2a33153522a8afbc3d215cfeb
Igor Santos <igorag314@gmail.com>
2021-05-21 22:33:01 -0300

    Sistema de Calibração para imagem maiores

--- map.py
+++ map.py
@@ -10,9 +10,11 @@ class mapa(Frame):
 
     def __init__(self):
         
+        self.difX = 0
+        self.difY = 0
+
         self.window = Tk()
         self.window.title('MAPEAMENTO')
-        self.window.geometry('250x250')
 
         lbl = Label(text="MAPEAMENTO DA CIDADE:")
         lbl.pack()
@@ -55,27 +57,80 @@ class mapa(Frame):
             etLink.delete(0, END)
             etTitulo.delete(0, END)
 
+        #CALIBRAGEM DE POSICIONAMENTO DA IMAGEM
+        lblCalibragem = Label(text='Calibragem:')
+        lblCalibragem.pack(pady=15)
+
+        lblCalibragemX = Label(text="Digite o X:")
+        lblCalibragemX.pack()
+
+        self.etCalibX = Entry()
+        self.etCalibX.pack()
+
+        lblCalibragemY = Label(text="Digite o Y:")
+        lblCalibragemY.pack()
+
+        self.etCalibY = Entry()
+        self.etCalibY.pack()
+
+        btCod = Button(text='CALIBRAR', command=lambda: calib())
+        btCod.pack()
+
+        def calib():
+
+            x = self.etCalibX.get().split()
+            y = self.etCalibY.get().split()
+
+            print(x)
+            print(y)
+
+            self.difX = int(x[0]) - int(x[1])
+            self.difY = int(y[0]) - int(y[1])
+
+            print(self.difX)
+            print(self.difY)
+
+            print('CALIBRADO COM SUCESSO !')
+
+            self.etCalibX.delete(0, END)
+            self.etCalibY.delete(0, END)
+
         #AO PRESSIONAR P DISPARAR O EVENTO
-        self.window.bind("<KeyPress-p>", self.keyPressed)
+        self.window.bind("<F1>", self.keyPressed)
+        self.window.bind("<KeyPress-c>", self.keyPressed)
 
         self.window.mainloop()
 
-    def queryMousePosition(self):
+    def queryMousePosition(self, reset):
+
+        if reset:
+            #RESETAR CALIBRAGEM
+            self.difX = 0
+            self.difY = 0
+
         pt = POINT()
         windll.user32.GetCursorPos(byref(pt))
-        return { "x": pt.x-5, "y": pt.y+510 }
+        return { "x": pt.x - self.difX, "y": pt.y - self.difY }
 
     def keyPressed(self, event):
         l = event.keysym
 
-        if l == 'p':
+        if l == 'F1':
             #ATIVAR EVENTO
-            pos = self.queryMousePosition()
+            pos = self.queryMousePosition(False)
 
             #EXIBIR POSIÇÃO SELECIONADA
             self.lblPosAtual['text'] = "X=" + str(pos["x"]) + " Y:" + str(pos["y"])
 
             #GUARDAR POSIÇÃO
-            self.etPos.insert(0, str(pos["x"]) + "," + str(pos["y"]) + ",")
+            self.etPos.insert(END, str(pos["x"]) + "," + str(pos["y"]) + ",")
+
+        elif l == 'c':
+            #ATIVAR EVENTO
+            pos = self.queryMousePosition(True)
+
+            #INSERIR POSIÇÕES
+            self.etCalibX.insert(0, str(pos["x"]))
+            self.etCalibY.insert(0, str(pos["y"]))
 
 mapa()
